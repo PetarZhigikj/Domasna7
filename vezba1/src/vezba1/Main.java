1c6af0b7b37ec6e0e38c688864582523de438854
unknown <zigicpetar12@gmail.com>
2022-12-21 23:44:21 +0100

    Domasna Rabota 7

---
+++ vezba1/src/vezba1/Main.java
@@ -0,0 +1,23 @@
+package vezba1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> lekovi = new ArrayList<String>();
+        lekovi.add("Analgin");
+        lekovi.add("Lineks");
+        lekovi.add("Paracetamol");
+
+        if(lekovi.get(2).equals("Paracetamol")) {
+            System.out.println("Lekot pomaga za namaluvanje na telesnata temperatura.");
+        }
+        else {
+            System.out.println("Namenata na lekot e nepoznata.");
+        }
+
+    }
+
+}
---
+++ vezba4/src/vezba4/Main.java
@@ -0,0 +1,20 @@
+package vezba4;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Set<String> set1 = new HashSet<String>();
+
+        set1.add("Eden");
+        set1.add("Dva");
+        set1.add("Tri");
+        set1.add("Cetiri");
+        set1.add("Pet");
+
+        System.out.println(set1);
+    }
+
+}
